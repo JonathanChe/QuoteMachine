d9910391ee3b0bdf3820a7c686bd299aa3b676bd
Jonathan <jonathan.che@outlook.com>
2019-02-23 10:59:14 -0800

    included animations for unveiling quotes, work on rwd

--- src/Quote.jsx
+++ src/Quote.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const QuoteContainer = styled.div`
+const Container = styled.div`
   min-height: 100vh;
   width: 40%;
   display: flex;
@@ -20,20 +20,194 @@ const QuoteContainer = styled.div`
   }
 `;
 
+const QuoteContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: block;
+  position: relative;
+  overflow: hidden;
+`;
+
+const ImgContainer = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%,-50%);
+  z-index: 2;
+
+  img {
+    -webkit-filter: drop-shadow(-4px 5px 5px rgba(0,0,0,0.6));
+    filter: drop-shadow(-4px 5px 5px rgba(0,0,0,0.6));
+    height: 200px;
+    animation: image-slide 4s cubic-bezier(.5,.5,0,1);
+    animation-fill-mode: forwards;
+  }
+
+  /* Slides the image from left (-250px) to right (150px) */
+  @keyframes image-slide {
+    0% { transform: translateX(-250px) scale(0); }
+    60% { transform: translateX(-250px) scale(1); }
+    90% { transform: translateX(150px) scale(1); }
+    100% { transform: translateX(150px) scale(1); }
+  }
+
+  @media screen and (min-width: 1000px) {
+    @keyframes image-slide {
+      0% {
+        transform: translatex(-250px) scale(0);
+      }
+      60% {
+        transform: translatex(-250px) scale(1);
+      }
+      90% {
+        transform: translatex(150px) scale(1);
+      }
+      100% {
+        transform: translatex(150px) scale(1);
+      }
+    }
+  }
+
+  @media screen and (max-width: 1000px) {
+    @keyframes image-slide {
+      0% {
+        transform: translatex(-150px) scale(0);
+      }
+      60% {
+        transform: translatex(-150px) scale(1);
+      }
+      90% {
+        transform: translatex(120px) scale(1);
+      }
+      100% {
+        transform: translatex(120px) scale(1);
+      }
+    }
+  }
+
+  @media screen and (max-width: 450px) {
+    @keyframes image-slide {
+      0% {
+        transform: translatex(-80px) scale(0);
+      }
+      60% {
+        transform: translatex(-80px) scale(1);
+      }
+      90% {
+        transform: translatex(80px) scale(1);
+      }
+      100% {
+        transform: translatex(80px) scale(1);
+      }
+    }
+  }
+`;
+
+const CoolQuote = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%,-50%);
+  z-index: 1;
+  margin-left: -100px;
+
+  p {
+    font-size: 24px;
+    font-family: sans-serif;
+  }
+
+  .fading-effect {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    width: 100%;
+    background: white;
+    -webkit-animation: text-slide 4s cubic-bezier(0.5, 0.5, 0, 1);
+    animation: text-slide 4s cubic-bezier(0.5, 0.5, 0, 1);
+    animation-fill-mode: forwards;
+    -webkit-animation-fill-mode: forwards;
+  }
+
+  @keyframes text-slide {
+    0% { width: 100%; }
+    60% { width: 100%; }
+    75%{ width: 0; }
+    100% { width: 0; }
+  }
+
+  @media screen and (min-width: 1000px) {
+    @keyframes text-slide {
+      0% {
+        width: 100%;
+      }
+      60% {
+        width: 100%;
+      }
+      77% {
+        width: 0;
+      }
+      100% {
+        width: 0;
+      }
+    }
+  }
+
+  @media screen and (max-width: 1000px) {
+    @keyframes text-slide {
+      0% {
+        width: 100%;
+      }
+      60% {
+        width: 100%;
+      }
+      77% {
+        width: 0;
+      }
+      100% {
+        width: 0;
+      }
+    }
+  }
+
+  @media screen and (max-width: 450px) {
+    @keyframes text-slide {
+      0% {
+        width: 100%;
+      }
+      60% {
+        width: 100%;
+      }
+      77% {
+        width: 0;
+      }
+      100% {
+        width: 0;
+      }
+    }
+  }
+`;
+
 const Quote = ({ quote, selected, loading }) => (
-  <QuoteContainer>
+  <Container>
     {loading ?
       (
       <p>Fetching...</p>
       )
       : (
-        <div>
-          <h3> { selected ? `Selected: ${selected}` : ''}</h3>
-          <p> {quote ? quote : ''} </p>
-        </div>
+        <QuoteContainer>
+          <ImgContainer>
+            <img src="https://purepng.com/public/uploads/large/purepng.com-curtainscurtainsdrapepiece-of-clothcovering-1701527923445lk8ec.png" alt="curtains-icon" />
+          </ImgContainer>
+          <CoolQuote>
+            <p> { selected ? `Selected: ${selected}` : 'Pick one!'}</p>
+            <p> {quote ? quote : ''} </p>
+            <div className="fading-effect" />
+          </CoolQuote>
+        </QuoteContainer>
       )
     }
-  </QuoteContainer>
+  </Container>
 );
 
 export default Quote;
